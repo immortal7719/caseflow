409e64f3cca857db4b445077e369f4ff6da75038
Alysson Barrera <alyssonbarrera@outlook.com>
2025-09-26 12:14:00 -0300

    ✨ add lodash.isequal for deep comparison in GroupNode memoization and update useBaseFlowModel to handle node state changes

--- src/modules/flow/nodes/group-node/group-node.tsx
+++ src/modules/flow/nodes/group-node/group-node.tsx
@@ -1,4 +1,5 @@
 import type { NodeProps } from "@xyflow/react";
+import isEqual from "lodash.isequal";
 import { memo } from "react";
 import { useGroupNodeModel } from "./group-node-model";
 import { GroupNodeView } from "./group-node-view";
@@ -10,4 +11,27 @@ function GroupNodeComponent(props: GroupNodeProps) {
   return <GroupNodeView {...props} {...useGroupNodeModel(props)} />;
 }
 
-export const GroupNode = memo(GroupNodeComponent);
+export const GroupNode = memo(GroupNodeComponent, (prevProps, nextProps) => {
+  if (
+    prevProps.id !== nextProps.id ||
+    prevProps.selected !== nextProps.selected ||
+    prevProps.dragging !== nextProps.dragging ||
+    prevProps.deletable !== nextProps.deletable ||
+    prevProps.selectable !== nextProps.selectable ||
+    prevProps.draggable !== nextProps.draggable ||
+    prevProps.type !== nextProps.type ||
+    prevProps.zIndex !== nextProps.zIndex ||
+    prevProps.isConnectable !== nextProps.isConnectable ||
+    prevProps.width !== nextProps.width ||
+    prevProps.height !== nextProps.height ||
+    prevProps.positionAbsoluteX !== nextProps.positionAbsoluteX ||
+    prevProps.positionAbsoluteY !== nextProps.positionAbsoluteY ||
+    prevProps.sourcePosition !== nextProps.sourcePosition ||
+    prevProps.targetPosition !== nextProps.targetPosition ||
+    prevProps.zIndex !== nextProps.zIndex
+  ) {
+    return false;
+  }
+
+  return isEqual(prevProps.data, nextProps.data);
+});
